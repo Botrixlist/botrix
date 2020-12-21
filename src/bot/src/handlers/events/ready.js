ba0b7148b2881357630794b2c405188f84c9442d
Awish-Senpai <69074469+Awish-Senpai@users.noreply.github.com>
2020-12-21 11:02:06 +0530

    Update ready.js

--- src/bot/src/handlers/events/ready.js
+++ src/bot/src/handlers/events/ready.js
@@ -2,13 +2,13 @@ const chalk = require("chalk");
 
 module.exports = async (client) => {
   client.user.setPresence({
-    activity: { name: "https://botrix.cc/", type: "WATCHING" },
+    activity: { name: "https://botrix.cc/", type: "WATCHING" }, //You can change the activity here, or keep it that.
     status: "idle",
   });
 
   setInterval(() => {
     client.user.setPresence({
-      activity: { name: "https://botrix.cc/", type: "WATCHING" },
+      activity: { name: "https://botrix.cc/", type: "WATCHING" }, //You can change the activity here, or keep it that.
       status: "idle",
     });
     chalk.greenBright("[BOT UPDATE] The bot presence has been updated.");
