62b1c9aca3730b79ca777376b2a45e09105cfcde
Dylan J <dylan@teamtritan.wtf>
2021-01-03 20:47:47 -0500

    removing v1 bot

--- src/bslBot/events/ready.js
+++
@@ -1,9 +0,0 @@
-const { readdirSync } = require ("fs")
-const { join } = require ("path")
-const filePath2 = join(__dirname, "..", "events");
-const eventFiles2 = readdirSync(filePath2);
-const BotrixLogs = console.log;
-
-module.exports = (client) => {
-    client.user.setPresence({ activity: { name: 'https://botrix.cc/servers', type: "WATCHING" }, status: 'idle' })
-}
